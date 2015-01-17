1606a626a2a721b57399d99a1a640dec902e1e74
JLeske <jens.leske@gmx.de>
2015-01-17 09:07:02 +0100

    Starter kit added.

--- js/app.js
+++ js/app.js
@@ -2,31 +2,42 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
   // put your routes here
+  this.resource('index', {path: '/'});
   this.resource('about', {path: '/about'});
   this.resource('contact', {path: '/contact'});
 });
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+    return this.store.findAll('work');
+   }
 });
 
-App.AboutRoute = Ember.Route.extend({
-	model: function() {
-		return this.store.find('text');
+App.ApplicationAdapter = DS.FixtureAdapter.extend({
+
+});
+
+App.Work = DS.Model.extend({
+	title: DS.attr('string'),
+	client: DS.attr('string'),
+	url: DS.attr('string')
+});
+
+App.Work.FIXTURES =
+[
+	{
+		id: 1,
+		title: 'ZipJet Driver App',
+		client: 'ZipJet',
+		url: 'http://www.zipjet.com'
+	},
+	{
+		id: 2,
+		title: 'Augsburger Allgemeine iOS App',
+		client: 'Augsburger Allgemeine',
+		url: 'http://www.augsburger-allgemeine.de'
 	}
-});
-
-App.AboutController = Ember.ObjectController.extend({
-
-});
-
-App.About = DS.Model.extend({
-	text: DS.attr('string')
-});
+]
 
 
-App.About.FIXTURES = {
-	text: 'Bla bla bla.'
-};
+
